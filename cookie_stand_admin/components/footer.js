299994e4c50759a4e24a9ba9667953ee1c104e42
qusaiqishta <qeshtaqusai0@gmail.com>
2021-08-08 18:41:08 +0300

    done

--- cookie_stand_admin/components/footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer(props) {
-
-  return (
-      <footer className="absolute bottom-0 w-full px-5 py-2 bg-green-500">
-          <p>{props.cookieStandArr.length} Locations World Wide</p>
-      </footer>
-  )
-}
--- cookie_stand_admin/pages/index.js
+++ cookie_stand_admin/pages/index.js
@@ -1,71 +1,38 @@
-import Head from '../components/head'
-import Header from '../components/header'
-import Form from '../components/main'
-import Table from '../components/table'
-import Footer from '../components/footer'
-import Login from '../components/loginForm'
-import { useState } from 'react'
-import axios from 'axios';
+import axios from "axios";
+import { useState } from "react";
 
 
-const baseUrl='https://cookie-stand-api.herokuapp.com/';
-const tokenUrl = baseUrl + '/api/token/';
+import CookieStandAdmin from "../components/CookieStandAdmin"
+import LoginForm from '../components/LoginForm'
+
+const baseUrl = 'https://cookie-stand-api.herokuapp.com';
+const tokenUrl= baseUrl +'/api/token/';
+const refreshToken= baseUrl + '/api/token/refresh';
 
 
+export default function Home(props){
 
-export default function Home() {
-  const [totalPerHour, setTotalPerHour] = useState(0);
-  const [cookieStands, setCookieStands] = useState([])
-  const [isLogin, setIsLogin] = useState(false) // Extra
+  const [token, setToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
+
+  async function getToken(credentials){
+    const fetchedToken = await axios.post(tokenUrl, credentials);
+    setToken(fetchedToken.data.access);
+    setRefreshToken(fetchedToken.data.refresh);
+  }
+
+  function signouthandler(){
+    setToken('')
+  }
+
+
+  function loginHandler(credentials){
+    getToken(credentials)
+  }
+
   
-  async function saveInfo(e){
-    e.preventDefault();
-    const loginInfo = {
-        username : e.target.username.value ,
-        password : e.target.password.value,
-      };
-      getData(loginInfo)
-    }
+  if (token) return <CookieStandAdmin  token={token} signouthandler={signouthandler} />
+ 
+  return  <LoginForm loginHandler={loginHandler} />
 
-  async function login(loginInfo) {
-    return axios.post(`${baseUrl}api/token/`, loginInfo);
-  }
-
-  async function fetchStands(loginInfo) {
-    try{
-    const tokenResponse = await login(loginInfo)
-    const { refresh, access: token } = tokenResponse.data;
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-    const response = await axios.get(`${baseUrl}api/v1/cookie_stands/`, config);
-    setIsLogin(true)
-    return response.data;
-  }catch(error){
-    console.log(error)
-  }
-    
-  }
-
-  async function getData(loginInfo) {
-    setCookieStands(await fetchStands(loginInfo));
-  }
-
-  if (isLogin){
-  
-  return (
-    <div>
-    <Head title ="Cookies Stand Admin"/>
-    <Header title ="Cookies Stand Admin"/>
-    <Form cookieStands={cookieStands} setCookies={setCookieStands} totalPerHour={totalPerHour} setTotalPerHour={setTotalPerHour}  />
-    <Table stands={[cookieStands, totalPerHour]}/>
-    <Footer cookieStandArr={cookieStands}/>
-  </div>
-  )}else{
-    return(
-      <Login saveInfo ={saveInfo} />
-    )
-  }
 }
